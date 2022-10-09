51ec3e16268b614358d1ce395be13968b50f89fa
wanmaoyuan <wanmaoyuan@outlook.com>
2022-10-09 07:03:45 +0000

    add server.go -> control grpc server run and stop

---
+++ pkg/server/server.go
@@ -0,0 +1,16 @@
+package server
+
+type KafkaServer struct {
+}
+
+func NewKafkaServer() *KafkaServer {
+	return &KafkaServer{}
+}
+
+func (server *KafkaServer) Run() {
+
+}
+
+func (server *KafkaServer) Stop() {
+
+}

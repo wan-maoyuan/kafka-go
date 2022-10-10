1ad2f5f0c82cadb84a5807a9cfac122c1cfde9b8
wanmaoyuan <wanmaoyuan@outlook.com>
2022-10-10 08:56:43 +0000

    service implement NewKafkaService, CreateTopic & DeleteTopic function

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -2,21 +2,56 @@ package service
 
 import (
 	"context"
+	"os"
+	"path/filepath"
 
 	pb "github.com/wan-maoyuan/kafka-go/api/kafka"
+	"github.com/wan-maoyuan/kafka-go/pkg/topic"
+)
+
+var (
+	dataDir       = "./data"
+	topicFilePath = filepath.Join(dataDir, "topic")
 )
 
 type KafkaService struct {
 	pb.UnimplementedKafkaServer
+	topicCtl *topic.Topic
+}
+
+func init() {
+	if _, err := os.Stat(dataDir); err != nil {
+		os.Mkdir(dataDir, 0755)
+	}
+}
+
+func NewKafkaService() (*KafkaService, error) {
+	t, err := topic.NewTopic(topicFilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	return &KafkaService{
+		topicCtl: t,
+	}, nil
 }
 
 func (s *KafkaService) CreateTopic(_ context.Context, req *pb.CreateTopicRequest) (*pb.CreateTopicResponse, error) {
+	s.topicCtl.Create(req.TopicName)
 
-	return nil, nil
+	return &pb.CreateTopicResponse{
+		IsCreated: true,
+		Message:   "",
+	}, nil
 }
 
 func (s *KafkaService) DeleteTopic(_ context.Context, req *pb.DeleteTopicRequest) (*pb.DeleteTopicResponse, error) {
-	return nil, nil
+	s.topicCtl.Delete(req.TopicName)
+
+	return &pb.DeleteTopicResponse{
+		IsDeleted: true,
+		Message:   "",
+	}, nil
 }
 
 func (s *KafkaService) GetAllTopics(_ context.Context, req *pb.GetAllTopicsRequest) (*pb.GetAllTopicsResponse, error) {

88d26ad1dd27e1f63e9e34e903e8071b15f59e7e
wanmaoyuan <wanmaoyuan@outlook.com>
2022-10-10 08:42:22 +0000

    implement topic struct function

--- pkg/topic/topic.go
+++ pkg/topic/topic.go
@@ -1,19 +1,89 @@
 package topic
 
-type topic struct{}
+import (
+	"bufio"
+	"os"
+	"path/filepath"
+	"strings"
 
-func NewTopic() (*topic, error) {
-	return nil, nil
+	"github.com/robfig/cron"
+	"github.com/sirupsen/logrus"
+)
+
+type topic struct {
+	path     string
+	topicMap map[string]struct{}
 }
 
-func (t *topic) Create(name string) error {
-	return nil
+func NewTopic(path string) (*topic, error) {
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0755)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	m := make(map[string]struct{})
+	reader := bufio.NewReader(f)
+	stop := false
+	for !stop {
+		line, isPrefix, err := reader.ReadLine()
+		if err != nil {
+			break
+		}
+
+		if strings.Trim(string(line), " ") != "" {
+			m[string(line)] = struct{}{}
+		}
+		stop = isPrefix
+	}
+
+	return &topic{
+		path:     f.Name(),
+		topicMap: m,
+	}, nil
+}
+
+func (t *topic) Create(name string) {
+	t.topicMap[name] = struct{}{}
 }
 
 func (t *topic) Delete(name string) {
-
+	delete(t.topicMap, name)
 }
 
-func (t *topic) Close() error {
-	return nil
+// 定时将内存中的数据同步到硬盘上
+func (t *topic) CronSync() {
+	c := cron.New()
+
+	c.AddFunc("* 5 * * * *", func() { // 每个5分钟同步一次
+		topicSync(t.path, t.topicMap)
+	})
+
+	c.Start()
+	select {}
+}
+
+func (t *topic) Close() {
+	topicSync(t.path, t.topicMap)
+}
+
+func topicSync(path string, m map[string]struct{}) {
+	dir := filepath.Dir(path)
+	tmp := filepath.Join(dir, ".topic.tmp")
+	tmpFile, err := os.OpenFile(tmp, os.O_CREATE|os.O_RDWR, 0755)
+	if err != nil {
+		logrus.Errorf("create and open topic tmp file error: %v", err)
+		return
+	}
+
+	for key := range m {
+		tmpFile.WriteString(key + "\n")
+	}
+	tmpFile.Close()
+
+	if err := os.Remove(path); err != nil {
+		return
+	}
+
+	os.Rename(tmpFile.Name(), path)
 }
